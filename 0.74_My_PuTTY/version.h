c4b14921cca656c9b3c769f57c574cff886629c9
cyd01 <cyrildupont71@gmail.com>
2021-01-11 19:30:11 +0100

    Tagging the 0.74.4.4 release of the KiTTY project.

--- 0.74_My_PuTTY/version.h
+++ 0.74_My_PuTTY/version.h
@@ -1,5 +1,5 @@
 #define RELEASE 0.74
 #define TEXTVER "Release 0.74"
 #define SSHVER "-Release-0.74"
-#define BINARY_VERSION 0,74,4,3
+#define BINARY_VERSION 0,74,4,4
 #define SOURCE_COMMIT "unavailable"
