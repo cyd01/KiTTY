ab2810a0e09de1aba4c723674df55870efdc02cd
cyd01 <cyrildupont71@gmail.com>
2023-05-23 19:23:03 +0200

    Tagging the 0.76.1.12 release of the KiTTY project.

--- 0.76b_My_PuTTY/version.h
+++ 0.76b_My_PuTTY/version.h
@@ -1,5 +1,5 @@
 #define RELEASE 0.76
 #define TEXTVER "Release 0.76"
 #define SSHVER "-Release-0.76"
-#define BINARY_VERSION 0,76,1,11
+#define BINARY_VERSION 0,76,1,12
 #define SOURCE_COMMIT "unavailable"
